98483c99414b592810dc15d79d2d7e80b5194b54
Fadhil <musyaffa262@gmail.com>
2024-02-05 14:01:14 +0700

    localStorage added

--- lib/store.ts
+++ lib/store.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { v4 as uuid } from 'uuid'
+import { persist } from 'zustand/middleware'
 
 export type Status = 'TODO' | 'IN_PROGRESS' | 'DONE'
 
@@ -21,40 +22,45 @@ export type Actions = {
   updateTask: (id: string, status: string) => void
 }
 
-export const useTaskStore = create<State & Actions>()(set => ({
-  tasks: [],
-  draggedTask: null,
-  addTask: (title: string, description?: string) => {
-    set(state => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: uuid(),
-          title: title,
-          description: description,
-          status: 'TODO'
-        }
-      ]
-    }))
-  },
-  dragTaskUpdate: (id: string | null) => {
-    set({ draggedTask: id })
-  },
-  removeTask: (id: string) => {
-    set(state => ({
-      tasks: state.tasks.filter(task => task.id !== id)
-    }))
-  },
-  updateTask: (id: string, status: string) => {
-    set(state => ({
-      tasks: state.tasks.map(task =>
-        task.id === id
-          ? {
-              ...task,
-              status
+export const useTaskStore = create<State & Actions>()(
+  persist(
+    set => ({
+      tasks: [],
+      draggedTask: null,
+      addTask: (title: string, description?: string) => {
+        set(state => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: uuid(),
+              title: title,
+              description: description,
+              status: 'TODO'
             }
-          : task
-      )
-    }))
-  }
-}))
+          ]
+        }))
+      },
+      dragTaskUpdate: (id: string | null) => {
+        set({ draggedTask: id })
+      },
+      removeTask: (id: string) => {
+        set(state => ({
+          tasks: state.tasks.filter(task => task.id !== id)
+        }))
+      },
+      updateTask: (id: string, status: string) => {
+        set(state => ({
+          tasks: state.tasks.map(task =>
+            task.id === id
+              ? {
+                  ...task,
+                  status
+                }
+              : task
+          )
+        }))
+      }
+    }),
+    { name: 'task-store', skipHydration: true }
+  )
+)
